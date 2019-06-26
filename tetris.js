2228474b9fe3764fbbb066c64774d8c7a9baca54
Matthew Woodley <matt@mattwoodley.net>
2019-06-26 13:26:18 +0100

    changed matrix wording to board, changed board size to 10 columns

--- tetris.js
+++ tetris.js
@@ -1,18 +1,19 @@
 const canvas = document.querySelector('#tetris');
 const context = canvas.getContext('2d');
 
+//getContext allows us to use methods and access properties in JavaScript.
 //Increase size of the context within the canvas (the tetris pieces)
 context.scale(20, 20);
 
 /*
 //chooses the colour for the background of the canvas (black)
-{moved context.fillStyle to draw() function in order to wipe clean the frame before drawMatrix() is called}
+{moved context.fillStyle to draw() function in order to wipe clean the frame before drawBoard() is called}
 context.fillStyle = '#000';
 */
 
 /*
 //Draws a filled rectangle whose starting point is at (x, y) and whose size is specified by width and height. The fill style is determined by the current fillStyle attribute.
-{moved context.fillRect to draw() function in order to wipe clean the frame before drawMatrix() is called}
+{moved context.fillRect to draw() function in order to wipe clean the frame before drawBoard() is called}
 context.fillRect(0, 0, canvas.width, canvas.height);
 */
 
@@ -42,10 +43,10 @@ function between(score, min, max) {
 }
 
 function collide(arena, player) {
-  const [matrix, offset] = [player.matrix, player.pos];
-  for (let y = 0; y < matrix.length; y++) {
-    for (let x = 0; x < matrix[y].length; x++) {
-      if (matrix[y][x] != 0 && //if player position is not 0,0 then the player has moved and might collide
+  const [board, offset] = [player.board, player.pos];
+  for (let y = 0; y < board.length; y++) {
+    for (let x = 0; x < board[y].length; x++) {
+      if (board[y][x] != 0 && //if player position is not 0,0 then the player has moved and might collide
         (arena[y + offset.y] && //if arena row doesn't exist then potential collision
         arena[y + offset.y][x + offset.x]) !== 0) { //if arena row and column don't exist and aren't 0, then must collide
           //return true if collision found.
@@ -57,12 +58,12 @@ function collide(arena, player) {
   return false;
 }
 
-function createMatrix(width, height) {
-  const matrix = [];
+function createboard(width, height) {
+  const board = [];
   while (height--) {
-    matrix.push(new Array(width).fill(0))
+    board.push(new Array(width).fill(0))
   }
-  return matrix;
+  return board;
 }
 
 function createPiece(type) {
@@ -114,16 +115,18 @@ function createPiece(type) {
 function draw() {
   context.fillStyle = '#000';
   context.fillRect(0, 0, canvas.width, canvas.height);
+  context.strokeStyle = 'white';
+  context.strokeRect(0, 0, canvas.width, canvas.height);
 
-  drawMatrix(arena, {x: 0, y: 0});
-  drawMatrix(player.matrix, player.pos);
+  drawBoard(arena, {x: 0, y: 0});
+  drawBoard(player.board, player.pos);
 }
 
-//this takes in a piece (matrix) and fills each 'rectangle' red
+//this takes in a piece (board) and fills each 'rectangle' red
 
 //!!!offset needs more explanation!!!
-function drawMatrix(matrix, offset) {
-  matrix.forEach((row, y) => {
+function drawBoard(board, offset) {
+  board.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
         context.fillStyle = colours[value];
@@ -163,7 +166,7 @@ function drawMatrix(matrix, offset) {
 //   }
 // }
 
-//DEV SCORE
+//DEV SCORES FOR TESTING
 function levelUp() {
   if (player.score <= 19) {
     dropInterval = 1000;
@@ -190,7 +193,7 @@ function levelUp() {
 
 //this function merges the player's position into the arena's empty table of arrays - so all of the arena's arrays of 0s will have a tetris piece of 1s within the table after they merge.
 function merge(arena, player) {
-  player.matrix.forEach((row, y) => {
+  player.board.forEach((row, y) => {
     row.forEach((value, x) => {
       if (value !== 0) {
         arena[y + player.pos.y][x + player.pos.x] = value;
@@ -236,9 +239,9 @@ function playerMove(dir) {
 //upon tetris piece being placed, playerReset() chooses a new piece at random and starts at the top of the arena.
 function playerReset() {
   const pieces = 'IJLOSTZ';
-  player.matrix = createPiece(pieces[Math.floor(Math.random() * Math.floor(pieces.length))]);
+  player.board = createPiece(pieces[Math.floor(Math.random() * Math.floor(pieces.length))]);
   player.pos.y = 0;
-  player.pos.x = (Math.floor(arena[0].length / 2)) - (Math.floor(player.matrix[0].length / 2));
+  player.pos.x = (Math.floor(arena[0].length / 2)) - (Math.floor(player.board[0].length / 2));
   //if collide upon a reset then the game is over and the arena is cleared of tetris pieces.
   if (collide(arena, player)) {
     arena.forEach(row => row.fill(0));
@@ -253,7 +256,7 @@ function playerReset() {
 function playerRotate(dir) {
   const pos = player.pos.x; //Store player's X position before rotation.
   let offset = 1; //Assign an offset to use for later.
-  rotate(player.matrix, dir); //Perform the actual matrix rotation.
+  rotate(player.board, dir); //Perform the actual board rotation.
 
   //If there is a collision immediately after rotating then the rotation was illegal.
   //But we allow rotation if the piece can fit when moved out from the wall.
@@ -261,8 +264,8 @@ function playerRotate(dir) {
     //Thus we try and move the piece left/right until it no longer collides.
     player.pos.x += offset;
     offset = -(offset + (offset > 0 ? 1 : -1)); //Produces 1, -2, 3, -4, 5 etc.
-    if (offset > player.matrix[0].length) { //If we have tried to offset more than the piece width, we deem the rotation unsuccessful.
-      rotate(player.matrix, -dir); //Reset rotation.
+    if (offset > player.board[0].length) { //If we have tried to offset more than the piece width, we deem the rotation unsuccessful.
+      rotate(player.board, -dir); //Reset rotation.
       player.pos.x = pos; //Reset position.
       return;
     }
@@ -270,22 +273,22 @@ function playerRotate(dir) {
 }
 
 //rotation by transposing and then reversing.
-function rotate(matrix, dir) {
-  for (let y = 0; y < matrix.length; y++) {
+function rotate(board, dir) {
+  for (let y = 0; y < board.length; y++) {
     for (let x = 0; x < y; x++) {
       [
-        matrix[x][y],
-        matrix[y][x],
+        board[x][y],
+        board[y][x],
       ] = [
-        matrix[y][x],
-        matrix[x][y],
+        board[y][x],
+        board[x][y],
       ];
     }
   }
   if (dir > 0) {
-    matrix.forEach(row => row.reverse());
+    board.forEach(row => row.reverse());
   } else {
-    matrix.reverse();
+    board.reverse();
   }
 }
 
@@ -316,6 +319,21 @@ function update(time = 0) {
   requestAnimationFrame(update);
 }
 
+// squareSize = 20 pixels.
+const squareSize = 20;
+
+const row = 20;
+const column = 10;
+const vacant = 'black';
+
+
+
+function drawSquare(x, y, colour) {
+  context.fillStyle = colour;
+  context.fillRect(x*squareSize, y*squareSize, squareSize, squareSize);
+  context.strokeStyle = 'white';
+  context.strokeRect(x*squareSize, y*squareSize, squareSize, squareSize);
+}
 
 const colours = [
   null,
@@ -328,28 +346,28 @@ const colours = [
   'red'
 ]
 
-const arena = createMatrix(12, 20);
-// console.log(arena); [Array(12) /*repeated 20 times with each array simply holding a 0*/]
+const arena = createBoard(10, 20);
+// console.log(arena); [Array(10) /*repeated 20 times with each array simply holding a 0*/]
 // console.table(arena); this shows the same result but in a nice table format
 
 //Player object that includes the player's position
 const player = {
   pos: {x: 0, y: 0},
-  matrix: null,
+  board: null,
   score: 0,
   level: 1,
 }
 
 /*
-//This logs the matrix of tetris pieces so that I can see the column arrays and row arrays of the pieces.
-console.table(player.matrix);
-console.log(player.matrix);
-console.table(player.matrix[0]);
-console.log(player.matrix[0]);
-console.log(player.matrix);
-console.log(player.matrix[0].length);
-console.log(player.matrix[1].length);
-console.log(player.matrix[2].length);
+//This logs the board of tetris pieces so that I can see the column arrays and row arrays of the pieces.
+console.table(player.board);
+console.log(player.board);
+console.table(player.board[0]);
+console.log(player.board[0]);
+console.log(player.board);
+console.log(player.board[0].length);
+console.log(player.board[1].length);
+console.log(player.board[2].length);
 */
 
 //key presses down result in moving or rotating tetris piece.
