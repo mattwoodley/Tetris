87b26a65fba792eadd38ae5c2e5f7af67035a158
m17woodley <m17woodley@gmail.com>
2021-01-27 20:33:59 +0000

    modulised pieces, createTetromino, tetrominoColors

---
+++ js/modules/tetrominos.js
@@ -0,0 +1,61 @@
+const pieces = 'IJLOSTZ';
+
+const createTetromino = (type) => {
+  if (type === 'I') {
+    return [
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0],
+      [0, 1, 0, 0]
+    ]
+  } else if (type === 'J') {
+    return [
+      [0, 2, 0],
+      [0, 2, 0],
+      [2, 2, 0]
+    ]
+  } else if (type === 'L') {
+    return [
+      [0, 3, 0],
+      [0, 3, 0],
+      [0, 3, 3]
+    ]
+  } else if (type === 'O') {
+    return [
+      [4, 4],
+      [4, 4]
+    ]
+  } else if (type === 'S') {
+    return [
+      [0, 5, 5],
+      [5, 5, 0],
+      [0, 0, 0]
+    ]
+  } else if (type === 'T') {
+    return [
+      [0, 0, 0],
+      [6, 6, 6],
+      [0, 6, 0],
+    ]
+  } else if (type === 'Z') {
+    return [
+      [7, 7, 0],
+      [0, 7, 7],
+      [0, 0, 0]
+    ]
+  }
+}
+
+// colors: cyan, blue, orange, yellow, green, pink, red
+const tetrominoColors = [
+  null,
+  '#00FFFF',
+  '#0084ff',
+  '#FFA500',
+  '#FFFF00',
+  '#00e800',
+  '#f200f2',
+  '#FF0000'
+]
+
+export { pieces, createTetromino, tetrominoColors };
